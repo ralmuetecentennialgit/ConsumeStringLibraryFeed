3995c9a158fd21f26c4cac4ec9bf198f1c2bd1cd
Rowel Almuete <ralmuete@my.centennialcollege.ca>
2023-12-01 21:10:32 -0500

    Initial commit

---
+++ ConsumeStringLibraryFeed/Program.cs
@@ -0,0 +1,31 @@
+﻿using UtilityLibraries;
+
+namespace ConsumeStringLibraryFeed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "Hello, World!";
+            var newhello = "hello World!";
+
+            // Function using the StringLibrary
+            void CheckAndPrintStartsWithUpper(string str)
+            {
+                if (StringLibrary.StartsWithUpper(str))
+                {
+                    Console.WriteLine($"{str} starts with an uppercase string");
+                }
+                else
+                {
+                    Console.WriteLine($"{str} starts with a lowercase string");
+                }
+            }
+
+            // Check the strings
+            CheckAndPrintStartsWithUpper(hello);
+            CheckAndPrintStartsWithUpper(newhello);
+
+        }
+    }
+}
